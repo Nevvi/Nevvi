8b52e625268780aa42e6980fb945bc2d09b7803e
Tyler Cobb <tyjcobb94@gmail.com>
2020-05-09 18:24:15 -0500

    Added terraform and development pipeline

---
+++ terraform/development/vars.tf
@@ -0,0 +1,14 @@
+variable "site_name" {
+  description = "My site"
+  default = "development.nevvi.net"
+}
+
+variable "site_zone" {
+  description = "My site zone"
+  default = "nevvi.net."
+}
+
+variable "environment" {
+  description = "Environment"
+  default = "development"
+}
