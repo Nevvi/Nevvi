e29e07f207505adc7a792d279e5e54d7d639664b
Tyler Cobb <tyjcobb94@gmail.com>
2020-05-10 10:48:23 -0500

    Added environment variable

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Nevvi!
+          Welcome to Nevvi - {process.env.REACT_APP_ENVIRONMENT}!
         </p>
         <a
           className="App-link"
