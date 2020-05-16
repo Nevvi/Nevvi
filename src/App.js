24e60f3ad9cf08c42a69deb983bd73066e2be34b
Tyler Cobb <tyjcobb94@gmail.com>
2020-05-16 09:26:53 -0500

    revert app.js changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-        <Route path="/" component={Home} />
-    </Router>
-  );
-};
-
-const Home = () => {
-  return (
-    <div>
-        <p>Environment - {process.env.REACT_APP_ENVIRONMENT}</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Nevvi - {process.env.REACT_APP_ENVIRONMENT}!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
 export default App;
