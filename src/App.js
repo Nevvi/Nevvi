e202a8e34ad4d1f4bed1df8f612430cdac411bd7
Tyler Cobb <tyjcobb94@gmail.com>
2020-05-17 17:49:36 -0500

    Added button to call backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Nevvi - {process.env.REACT_APP_ENVIRONMENT}!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    componentDidMount() {
+        console.log(process.env.REACT_APP_API_BASE_URL)
+    }
+
+    createAccount() {
+        axios.post(
+            `${process.env.REACT_APP_API_BASE_URL}/tyler-authentication/v1/register`,
+            {
+                username: 'tcobb2',
+                password: 'Passw0rd!'
+            }
+        ).then(function (response) {
+            console.log('RESPONSE', response)
+        }).catch(function (error) {
+            console.log('ERROR', error)
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Welcome to Nevvi - {process.env.REACT_APP_ENVIRONMENT}!
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+
+                    <button onClick={this.createAccount}>
+                        Create Account
+                    </button>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
--- terraform/development/vars.tf
+++
@@ -1,4 +0,0 @@
-variable "environment" {
-  description = "Environment"
-  default = "development"
-}
