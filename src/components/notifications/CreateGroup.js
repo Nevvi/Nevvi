0a677b75fc81eda68bce7ca2bd711f0c56ef49ec
Tyler Cobb <tyjcobb94@gmail.com>
2022-03-02 08:21:08 -0500

    Let user pass in expiration date when creating a group

--- src/components/notifications/CreateGroup.js
+++ src/components/notifications/CreateGroup.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {inject, observer} from "mobx-react";
 import {Box, Grid, TextField} from "@mui/material";
-import {LoadingButton} from "@mui/lab";
+import AdapterDayjs from "@mui/lab/AdapterDayjs";
+import {DatePicker, LoadingButton, LocalizationProvider} from "@mui/lab";
 
 class CreateGroup extends Component {
     render() {
@@ -9,29 +10,43 @@ class CreateGroup extends Component {
         const isDisabled = createNotificationGroupStore.groupName === ""
 
         return <Grid container direction="row" justifyContent="center" alignItems="center" rowSpacing={1}>
-            <Grid item xs={12}>
-                <TextField
-                    fullWidth
-                    variant="standard"
-                    id="group-name-input"
-                    label="Group Name"
-                    type="text"
-                    value={createNotificationGroupStore.groupName}
-                    onChange={(e) => createNotificationGroupStore.setGroupName(e.target.value)}
-                />
+            <Grid item xs={12} sm={6} md={3}>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <TextField
+                        fullWidth
+                        variant="standard"
+                        id="group-name-input"
+                        label="Group Name"
+                        type="text"
+                        value={createNotificationGroupStore.groupName}
+                        onChange={(e) => createNotificationGroupStore.setGroupName(e.target.value)}
+                    />
+                    <Box mt={3}>
+                        <DatePicker
+                            openTo="day"
+                            views={['year', 'month', 'day']}
+                            label="Expiration Date"
+                            value={createNotificationGroupStore.expirationDate}
+                            onChange={(expiration) => {
+                                createNotificationGroupStore.setExpirationDate(expiration.format("YYYY-MM-DD"));
+                            }}
+                            renderInput={(params) => <TextField {...params} helperText={null}/>}
+                        />
+                    </Box>
+                    <Box mt={3}>
+                        <LoadingButton
+                            size={"small"}
+                            variant="contained"
+                            color="primary"
+                            loading={createNotificationGroupStore.loading}
+                            disabled={isDisabled}
+                            onClick={() => createNotificationGroupStore.createGroup()}
+                        >
+                            Create Group
+                        </LoadingButton>
+                    </Box>
+                </LocalizationProvider>
             </Grid>
-            <Box mt={2}>
-                <LoadingButton
-                    size={"small"}
-                    variant="contained"
-                    color="primary"
-                    loading={createNotificationGroupStore.loading}
-                    disabled={isDisabled}
-                    onClick={() => createNotificationGroupStore.createGroup()}
-                >
-                    Create Group
-                </LoadingButton>
-            </Box>
         </Grid>
     }
 }
