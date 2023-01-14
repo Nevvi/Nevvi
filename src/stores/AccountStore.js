9c8cb1095f71bae0e273ec6738c07fe354a2e2f2
Tyler Cobb <tyjcobb94@gmail.com>
2023-01-14 10:00:46 -0500

    Send empty string for unit

--- src/stores/AccountStore.js
+++ src/stores/AccountStore.js
@@ -100,7 +100,9 @@ class AccountStore {
                 } else if (key === "address") {
                     // If updating address make sure we aren't sending over a partial one
                     Object.keys(this.updatedUser[key]).forEach(addressKey => {
-                        if (addressKey !== "unit" && !this.updatedUser[key][addressKey]) {
+                        if (addressKey === "unit" && !this.updatedUser[key][addressKey]) {
+                            this.updatedUser[key][addressKey] = ""
+                        } else if (!this.updatedUser[key][addressKey]) {
                             throw new SyntaxError(`All address fields are required for updating`)
                         }
                     })
